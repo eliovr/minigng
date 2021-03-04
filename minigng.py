5c586a2e410f257f190fc899b16f25f9452403ef
Elio Ventocilla <eliovr@gmail.com>
2021-03-04 11:33:28 +0100

    Second 'untangling' version. Draft code for anomaly detection.

--- minigng.py
+++ minigng.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import norm, multivariate_normal
 import random
 
 
@@ -7,6 +8,11 @@ class Unit:
         self.prototype = prototype
         self.error = error
         self.neighbors = set()
+
+        self.count = 0
+        self.var = None # variance.
+        self.cov = None # covariance.
+        self.labels = {}
 
     def distance_to(self, vector):
         return np.linalg.norm(vector - self.prototype)
@@ -37,7 +43,7 @@ class Edge:
 class MiniGNG:
     def __init__(self, n_epochs=50, sigma=100, max_units=100, eps_b=.2,
             eps_n=.006, max_edge_age=50, alpha=.5, d=.995,
-            untangle=False, min_net_size=3, shuffle=True):
+            untangle=False, min_net_size=3, shuffle=True, sample=1.0):
         """
         Parameters
         ----------
@@ -77,13 +83,16 @@ class MiniGNG:
 
         shuffle : boolean (default=True)
             Whether to shuffles the training data every epoch.
+
+        sample : float (default=1.0)
+            Sample a fraction of the training data for every epoch. Takes values
+            between 0 and 1 (where 1 = the entire dataset).
         """
 
         self.units = []
         self.edges = []
 
         self.n_epochs = n_epochs
-        self.shuffle = shuffle
         self.sigma = sigma
         self.max_units = max_units
         self.eps_b = eps_b
@@ -95,6 +104,10 @@ class MiniGNG:
         self.min_net_size = min_net_size
         self.signal_counter = 0
 
+        self.shuffle = shuffle
+        self.sample = sample
+        self.epsilon = .0   # anomaly threshold.
+        
 
     def init_model(self, xs):
         n = len(xs) - 1
@@ -107,9 +120,71 @@ class MiniGNG:
         self.edges.append(Edge(a, b))
 
 
+    def transform(self, xs, update_model_stats=False):
+        if len(self.units) == 0:
+            return None
+
+        else:
+            groups = {i: [] for i, _ in enumerate(self.units)}
+            prototypes = np.array([u.prototype for u in self.units])
+            labels = []
+
+            for i, x in enumerate(xs):
+                dists = np.linalg.norm(x - prototypes, axis=1)
+                unit_id = np.argmin(dists)
+                groups[unit_id].append(i)
+                labels.append(unit_id)
+
+            if update_model_stats:
+                for k, v in groups.items():
+                    unit = self.units[k]
+                    points_in_unit = xs[v]
+                    unit.count = len(points_in_unit)
+                    unit.var = np.var(points_in_unit, axis=0)
+                #     unit.cov = np.cov(points_in_unit, rowvar=False)
+
+                # ps = [self.multinorm_pdf(x) for x in xs]
+                # self.epsilon = np.min([p for p in ps if p > 0])
+
+            return labels
+
+
+    def norm_pdf(self, x):
+        prototypes = np.array([u.prototype for u in self.units])
+        dists = np.linalg.norm(x - prototypes, axis=1)
+        unit_id = np.argmin(dists)
+        unit = self.units[unit_id]
+        mean, std = unit.prototype, np.sqrt(unit.var)
+        p_x = [norm.pdf(v, u, s) for (v, u, s) in zip(x, mean, std)]
+        
+        return np.prod(p_x)
+
+
+    def multinorm_pdf(self, x):
+        prototypes = np.array([u.prototype for u in self.units])
+        dists = np.linalg.norm(x - prototypes, axis=1)
+        unit_id = np.argmin(dists)
+        unit = self.units[unit_id]
+        mean, cov = unit.prototype, unit.cov
+        
+        try:
+            return multivariate_normal.pdf(x, mean, cov)
+        except:
+            return float('nan')
+
+
+    def is_anomaly(self, x):
+        return self.norm_pdf(x) < self.epsilon
+
+
     def fit(self, xs):
         for _ in range(0, self.n_epochs):
             self.partial_fit(xs)
+            
+
+    def fit_transform(self, xs):
+        self.fit(xs)
+        return self.transform(xs, update_model_stats=True)
 
 
     def partial_fit(self, xs):
@@ -125,9 +200,12 @@ class MiniGNG:
         max_units = self.max_units
 
         signals = xs
-        if self.shuffle:
+
+        if self.shuffle or self.sample < 1.0:
             size = len(xs)
-            signals = xs[np.random.choice(np.arange(size), size)]
+            n_samples = size
+            if self.sample < 1.0: n_samples = int(size * self.sample)
+            signals = xs[np.random.choice(size, n_samples)]
 
         for signal in signals:
             self.signal_counter += 1
@@ -143,32 +221,15 @@ class MiniGNG:
             dist = distance[unit_a_id]
 
             ab_edge = None
-            _edges = []
 
+            # 3. Increment the age of all edges emanating from S1.
             for e in self.edges:
-                # 3. Increment the age of all edges emanating from S1.
                 if e.connects_unit(unit_a):
                     e.age += 1
 
                     if e.connects_unit(unit_b):
                         ab_edge = e
-
-                # 7. Remove edges with an age larger than maxAge. If this results in
-                # points having no emanating edges, remove them as well.
-                if e.age <= max_edge_age:
-                    _edges.append(e)
-
-                elif e.age > max_edge_age:
-                    e.source.neighbors.remove(e.target)
-                    e.target.neighbors.remove(e.source)
-
-                    if len(e.source.neighbors) <= 0:
-                        self.units.remove(e.source)
-
-                    if len(e.target.neighbors) <= 0:
-                        self.units.remove(e.target)
-
-            self.edges = _edges
+                        
 
             # 4. Add the squared distance between the input signal and
             # the nearest unit in input space to a local counter variable.
@@ -185,10 +246,30 @@ class MiniGNG:
             if not ab_edge is None:
                 ab_edge.age = 0
 
-            elif not self.untangle or self.no_curling(unit_a, unit_b):
+            elif not self.untangle or self.no_curling2(unit_a, unit_b):
                 unit_a.neighbors.add(unit_b)
                 unit_b.neighbors.add(unit_a)
                 self.edges.append(Edge(unit_a, unit_b))
+
+            # 7. Remove edges with an age larger than maxAge. If this results in
+            # points having no emanating edges, remove them as well.
+            _edges = []
+
+            for e in self.edges:
+                if e.age <= max_edge_age:
+                    _edges.append(e)
+
+                elif e.age > max_edge_age:
+                    e.source.neighbors.remove(e.target)
+                    e.target.neighbors.remove(e.source)
+
+                    if len(e.source.neighbors) <= 0:
+                        self.units.remove(e.source)
+
+                    if len(e.target.neighbors) <= 0:
+                        self.units.remove(e.target)
+
+            self.edges = _edges
 
             # 8. If the number of input signals generated so far is an integer
             # multiple of a parameter A, insert a new unit as follows.
@@ -228,6 +309,53 @@ class MiniGNG:
                 u.error *= d
 
 
+    def net_size_compare(self, node, size):
+        """
+        Checks the size of the network a node belongs to, against the parameter size.
+        Returns 1 if the size of the network is greater than "size", -1 if its lower,
+        and 0 if equal.
+        """
+
+        open_nodes = node.neighbors
+        closed_nodes = set()        
+        closed_nodes.add(node)
+
+        while len(open_nodes) > 0 and len(closed_nodes) <= size:
+            closed_nodes = closed_nodes | open_nodes
+            aux = set()
+            for n in open_nodes:
+                aux = aux | {ne for ne in n.neighbors if not ne in closed_nodes}
+                
+            open_nodes = aux
+        
+        n_nodes = len(closed_nodes)
+
+        if n_nodes < size: return -1
+        elif n_nodes > size: return 1
+        else: return 0
+
+    
+    def exists_path(self, a, b):
+        """
+        True if there's a path between a and b, False otherwise.
+        """
+
+        open_nodes = a.neighbors
+        closed_nodes = set()
+        exists = False
+
+        while len(open_nodes) > 0 and not exists:
+            exists = b in open_nodes
+            closed_nodes = closed_nodes | open_nodes
+            aux = set()
+            for n in open_nodes:
+                aux = aux | {ne for ne in n.neighbors if not ne in closed_nodes}
+                
+            open_nodes = aux
+            
+        return exists
+
+
     def no_curling(self, a, b):
         """
         Checks if connecting units a and b would 'curl' the network into a
@@ -249,6 +377,69 @@ class MiniGNG:
             bn = b.neighbors
 
             return (len(an.intersection(xn)) < 2) and (len(bn.intersection(xn)) < 2)
+
+        return False
+
+
+    def no_curling2(self, a, b):
+        """
+        Another curl check version with three rules. No curling takes place if:
+            - 'a' and 'b' have 2 neighbors in common, and those neighbors are not
+            connected to each other.
+            - 'a' and 'b' have 1 neighbor in common 'x', and there are no more than
+            1 path from 'a' to 'x', and from 'b' to 'x'.
+            - 'a' and 'b' belong to different networks.
+        """
+
+        bridges = a.neighbors.intersection(b.neighbors)
+        n_bridges = len(bridges)
+
+        if n_bridges == 2:
+            # no curling if the two common neighbors are not connected.
+            x, y = bridges
+            return len(x.neighbors.intersection(y.neighbors)) == 0
+
+        elif n_bridges == 1:
+            max_steps = 5
+            n_nodes, [bridge] = len(self.units), bridges
+            xi = self.units.index(bridge)
+            ai = self.units.index(a)
+            bi = self.units.index(b)
+
+            # construct edge matrix
+            edge_mat = np.zeros(n_nodes**2).reshape(n_nodes, n_nodes)
+
+            for e in self.edges:
+                u1, u2 = self.units.index(e.source), self.units.index(e.target)
+                edges[u1, u2] = 1
+                edges[u2, u1] = 1
+
+            # save edges of x (the bridge) without edges to a and b.
+            x_edges = edges[xi].copy()
+            x_edges[ai], x_edges[bi] = 0, 0
+            x_edges = [i for i, e in enumerate(x_edges) if e == 1]
+            # clear edges of x from the matrix
+            edge_mat[xi] = 0
+            edge_mat[:, xi] = 0
+            
+            # count paths from x to a and b, by restoring one edge of x at a time.
+            n_paths = {ai: 0, bi: 0}
+
+            for xe in x_edges:
+                aux = edge_mat.copy()
+                aux[xi, xe], aux[xe, xi] = 1, 1
+
+                # to avoid long execution times, just check paths up to 5 steps away.
+                for i in range(min(n_nodes-1, max_steps)):
+                    aux = aux.dot(aux)
+
+                if aux[xi, ai] != 0: n_paths[ai] += 1
+                if aux[xi, bi] != 0: n_paths[bi] += 1
+
+            return n_paths[ai] <= 1 and n_paths[bi] <= 1
+
+        elif n_bridges == 0:
+            return not self.exists_path(a, b)
 
         return False
 
