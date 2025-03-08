98c8d8065bcccb6771956d5882615263d64b1f31
Elio <vene@his.se>
2025-03-08 23:44:28 +0100

    Fix example.py

--- example.py
+++ example.py
@@ -1,23 +1,28 @@
 import networkx as nx
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from minigng import MiniGNG
 
+dataset = "/path/to/dataset.csv"
+output = "graph.png"
 
 # load data.
-df = pd.read_csv("/path/to/iris.csv", header=None)
+df = pd.read_csv(dataset, header=None)
+
+# assuming labels are in the first column.
 X = df.to_numpy(dtype=np.float32)[:, 1:]
 y = df[0].to_numpy()
 
 # init GNG.
 gng = MiniGNG(
-    max_units=150, 
-    n_epochs=25, 
+    max_units=100, 
+    n_epochs=10,
     max_edge_age=50, 
     sample=.2, 
     untangle=True, 
-    max_size_connect=0)
+    max_size_connect=3)
 
 # Traing GNG.
 gng.fit(X, y=y)
@@ -26,12 +31,20 @@ predictions, unit_ids = gng.predict(X, return_unit_ids=True)
 labels = dict(zip(unit_ids, predictions))
 
 # plot the graph using Networkx and Graphviz.
-edge_weight = lambda e: np.linalg.norm(e.source.prototype - e.target.prototype)
 nodes = [i for i, _ in enumerate(gng.units)]
-edges = [(gng.units.index(e.source), gng.units.index(e.target), {'weight': edge_weight(e)}) for e in gng.edges]
+edges = [(gng.units.index(e.source), gng.units.index(e.target), {}) for e in gng.edges]
 
 G = nx.Graph()
 G.add_nodes_from(nodes)
 G.add_edges_from(edges)
 
-nx.draw(G, pos=nx.nx_pydot.graphviz_layout(G, prog="neato"), node_size=50, labels=labels, node_color='white')
+layout = nx.nx_pydot.graphviz_layout(G, prog="neato")
+nx.draw(
+    G,
+    pos=layout,
+    node_size=50,
+    labels=labels,
+    node_color="white",
+    edge_color="gray",
+    width=.5)
+plt.savefig(output)
